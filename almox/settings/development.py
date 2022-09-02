130ac65cce1fe8c311c7279bd9ecc535211b8a47
Daniel Versoza Alves <daniel.alves@telavita.com.br>
2022-09-01 23:18:48 -0300

    Creates new settings files

--- almox/settings/development.py
+++ almox/settings/development.py
@@ -2,6 +2,10 @@ from .base import *
 
 DEBUG = True
 
-
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
+
+# Database settings
+DATABASES = {
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+}
