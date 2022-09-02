b28bced8e630de87a337da29424a5d498a5c0de3
dversoza <daniel.ve3@gmail.com>
2022-09-02 10:54:18 -0300

    Updating deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
-source .venv/bin/activate
+# Activate virtual environment
+if [[ "$VIRTUAL_ENV" = "" ]] then
+    source .venv/bin/activate
+fi
+
+# Install requirements
 pip install -r requirements.txt
+
+# Run migrations and collect static files
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+# Start server
 systemctl restart gunicorn.socket
 systemctl reload nginx.service
