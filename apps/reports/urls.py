70ff8add9fa24b3f73b9f23aceaeee7a5002c358
Daniel Versoza Alves <daniel.alves@telavita.com.br>
2022-09-24 18:59:45 -0300

    feat: stock summary tentative

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.reports.views import ExportCSVStands, ExportCSVProducts
+from apps.reports.views import ExportCSVStands, ExportCSVProducts, ExportStockCSV
 
 
 urlpatterns = [
     path("products/", ExportCSVProducts.as_view(), name="products-report"),
     path("stands/", ExportCSVStands.as_view(), name="stands-report"),
+    path("stock/", ExportStockCSV.as_view(), name="transactions-report"),
 ]
